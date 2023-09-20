3683c1859076ae8ea41b174aec0ac957e7d65af7
Nahuel Arrieta <nahuel.arrietamza@gmail.com>
2023-09-20 18:16:12 -0300

    add results

--- tp4-busquedas-informadas/code/main.py
+++ tp4-busquedas-informadas/code/main.py
@@ -10,7 +10,7 @@ def iterate(i, file):
     agent = Agent(env)
 
     agent.a_star_search()
-    write_csv("A*", agent.get_performance(), i, agent.is_end(agent.last_node), file)
+    write_csv("A*", agent.get_performance(), i, env.is_end(agent.last_node), file)
 
 def print_path():
     env = Enviroment(15, 15, 0.1)
@@ -19,11 +19,13 @@ def print_path():
     agent.a_star_search()
     env.print_environment(agent.get_path())
 
-file = open("./tp4-busquedas.informadas/informada-results.csv")
+file = open("./tp4-busquedas-informadas/informada-results.csv", "w")
 
 file.write("Algorithm_name,env_n,estates_n,solution_found")
 
 for i in range(30):
     iterate(i, file)
+
+file.close()
 
 print_path()
