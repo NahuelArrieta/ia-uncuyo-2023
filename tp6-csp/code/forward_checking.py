2d74ca130a0f5df66deee4997e319916ea7031ac
Nahuel Arrieta <nahuel.arrietamza@gmail.com>
2023-10-26 20:36:31 -0300

    add forward_checinkg

---
+++ tp6-csp/code/forward_checking.py
@@ -0,0 +1,27 @@
+from solution import *
+
+
+## use less_restrictive_value heuristic
+def do_forward_checking(solution: Solution):
+    print("_")
+    ## if solution is complete return True
+    if solution.is_complete():
+        return True
+    
+    ## get less restrictive row
+    less_restrictive_row = solution.get_less_restrictive_row()
+    row = less_restrictive_row.index
+    
+    ## try to add a queen in each column
+    for column in range(solution.n):
+        solution.add_queen(row, column)
+        if solution.is_valid():
+            if do_forward_checking(solution):
+                return True
+        solution.remove_queen(row, column)
+    return False
+
+def start_forward_checking(n):
+    solution = Solution(n)
+    do_forward_checking(solution)
+    return solution
