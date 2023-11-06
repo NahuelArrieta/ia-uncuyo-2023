6f230d2484720eeb751310898ac95a6dd47e73f3
Nahuel Arrieta <nahuel.arrietamza@gmail.com>
2023-11-06 19:35:47 -0300

    add files

--- tp5-busquedas-locales/code/main.py
+++ tp5-busquedas-locales/code/main.py
@@ -12,24 +12,29 @@ iterations_per_algo = 2
 ## Hill Climbing
 def iterate_hc():
     correct_solutions_hc = 0
+    file = open("./tp5-busquedas-locales/results/hill_climbing.csv", "w")
+    file.write("iterations,time\n")
     for _ in range(iterations_per_algo):
         hill_climbing = HillClimbing(board, max_iterations)
         if hill_climbing.get_last_solution().h == 0:
             correct_solutions_hc += 1
             iterations = hill_climbing.get_iterations()
             time = hill_climbing.get_time()
-
+            file.write(str(iterations) + "," + str(time) + "\n")
 
 
 ## Simulated Annealing
 def iterate_sa():
     correct_solutions_sa = 0
+    file = open("./tp5-busquedas-locales/results/simulated_annealing.csv", "w")
+    file.write("iterations,time\n")
     for _ in range(iterations_per_algo):
         simulated_annealing = SimulatedAnnealing(board, max_iterations)
         if simulated_annealing.get_last_solution().h == 0:
             correct_solutions_sa += 1
             iterations = simulated_annealing.get_iterations()
             time = simulated_annealing.get_time()
+            file.write(str(iterations) + "," + str(time) + "\n")
 
 
 ## Genetic Algorithm
@@ -39,12 +44,15 @@ def iterate_ga():
     c = crossover.Crossover("one_point")
     m = mutation.Mutation("swip_queens")
     correct_solutions_ga = 0
+    file = open("./tp5-busquedas-locales/results/genetic_algorithm.csv", "w")
+    file.write("iterations,time\n")
     for _ in range(iterations_per_algo):
         genetic_algorithm = GeneticAlgorithm(board, 100, max_iterations, s, r, c, m)
         if genetic_algorithm.get_last_solution().h == 0:
             correct_solutions_ga += 1
             iterations = genetic_algorithm.get_iterations()
             time = genetic_algorithm.get_time()
+            file.write(str(iterations) + "," + str(time) + "\n")
 
 
 iterate_ga()
